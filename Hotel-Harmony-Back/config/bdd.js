f4a2c718c8976b20a24a98a8b2ebfbbec3122f9e
Alireza <alirezaalavi7713@gmail.com>
2026-05-05 19:34:23 +0200

    Add SSL support for Aiven MySQL

--- Hotel-Harmony-Back/config/bdd.js
+++ Hotel-Harmony-Back/config/bdd.js
@@ -5,7 +5,8 @@ const bdd = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 bdd.getConnection()
